a079483b148a8c4fdaf00cf69ce442f84d54d0f9
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-08-23 01:53:26 +0700

    fixed add product with category, name group, size, edit product

---
+++ src/app/model/response-object.model.ts
@@ -0,0 +1,5 @@
+export interface ResponseObject {
+  status: string;
+  message: string;
+  data: any;
+}
--- src/app/service/category.service.ts
+++ src/app/service/category.service.ts
@@ -9,20 +9,29 @@ import { Category } from '../model/category.model';
 })
 export class CategoryService {
   REST_API = AppSettings.REST_API + '/category';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getCategoriesInAdmin(page: number, size: number): Observable<Category[]> {
+  getCategoriesInAdmin(
+    page: number,
+    size: number,
+    status: string
+  ): Observable<Category[]> {
     return this.http.get<Category[]>(
-      this.REST_API + '/all/admin?page=' + page + '&size=' + size,
+      this.REST_API +
+        '/all/admin?page=' +
+        page +
+        '&size=' +
+        size +
+        '&status=' +
+        status,
       { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
     );
   }
 
   updateCancelledStatus(id: number[]): Observable<boolean> {
-    return this.http.get<boolean>(
-      this.REST_API + '/editStatus?id=' + id,
-      { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
-    );
+    return this.http.get<boolean>(this.REST_API + '/editStatus?id=' + id, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    });
   }
 
   updateStatus(id: number, status: string): Observable<boolean> {
@@ -33,15 +42,14 @@ export class CategoryService {
   }
 
   saveCategory(category: Category): Observable<Category> {
-    return this.http.post<Category>(
-      this.REST_API , JSON.stringify(category),
-      { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
-    );
+    return this.http.post<Category>(this.REST_API, JSON.stringify(category), {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    });
   }
 
   existsCategoryName(categoryName: string): Observable<Boolean> {
     return this.http.get<Boolean>(
-      this.REST_API  + '/checkCategoryName?name=' + categoryName
+      this.REST_API + '/checkCategoryName?name=' + categoryName
     );
   }
 }
